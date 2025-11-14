cf08da216e73b420b30ca6a8da72615bcd8359cb
Najeeb Patoana <najeebpatoana007@gmail.com>
2025-11-14 16:18:12 +0500

    1.0

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { 
+        title: 'Home Page',
+        message: 'Welcome to Docker // Jenkins CI/CD!',
+        timestamp: new Date().toLocaleString()
+    });
+});
+
+
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', message: 'Server is running' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
