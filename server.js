e184fcee7b7cfdd368bafe00efc04307fefc1c87
Najeeb Patoana <najeebpatoana007@gmail.com>
2025-11-21 14:15:50 +0500

    changed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index', { 
         title: 'Home Page',
-        message: 'Najeeb Ullah Khan 47434 lab 6 Ok',
+        message: 'Najeeb Ullah Khan 47434 lab 6 change',
         timestamp: new Date().toLocaleString()
     });
 });
