4df0030b738c08f587d04b0e6b0b42c0f06b54e4
Najeeb Patoana <najeebpatoana007@gmail.com>
2026-01-12 00:57:32 +0500

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index', { 
         title: 'Home Page',
-        message: 'Najeeb Ullah Khan 47434 Devops',
+        message: 'Najeeb Ullah Khan 47434 Devops Lab',
         timestamp: new Date().toLocaleString()
     });
 });
