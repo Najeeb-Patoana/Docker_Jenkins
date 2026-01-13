44a5608c968ad2421ac519e699e0211f5a5e7ee4
Najeeb Patoana <najeebpatoana007@gmail.com>
2026-01-13 10:15:13 +0500

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index', { 
         title: 'Home Page',
-        message: 'Devops Lab today 123',
+        message: 'Devops Lab',
         timestamp: new Date().toLocaleString()
     });
 });
